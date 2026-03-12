abbc810cba02a0c9ae8237c8c3cb5df1f3777616
Hancer <hancermercedes@gmail.com>
2026-03-12 13:09:25 -0400

    Separating the command from the command handler to follow better practices.

--- NexusPOS.Application/Commands/DeleteProduct/DeleteProductCommand.cs
+++ NexusPOS.Application/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -5,17 +5,3 @@ namespace NexusPOS.Application.Commands.DeleteProduct;
 
 public record DeleteProductCommand(int Id) : IRequest<Unit>;
 
-public class DeleteProductCommandHandler(IUnitOfWork uow) : IRequestHandler<DeleteProductCommand, Unit>
-{
-    public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken ct)
-    {
-        var product = await uow.Products.GetByIdAsync(request.Id, ct)
-            ?? throw new KeyNotFoundException($"Product {request.Id} not found");
-
-        product.IsActive = false;
-        uow.Products.Update(product);
-        await uow.CommitAsync(ct);
-
-        return Unit.Value;
-    }
-}
